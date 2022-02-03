6c9ee66abb69a258e985fba1192686b6d70e61c0
lagonza8 <71752011+lagonza8@users.noreply.github.com>
2022-02-02 23:20:29 -0700

    Delete Admin.java

--- Admin.java
+++
@@ -1,7 +0,0 @@
-
-public class Admin {
-	public String name; 
-
-	// theoretically the Dev, Game, Reviews and Gamers since the Admin manages these, but for implementaiton purpocses does not make much sensse.
-
-}
